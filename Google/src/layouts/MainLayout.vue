c884f4d4ecbedd34f2d26a773e7eb3548c262435
Rzied <galaxyzied@gmail.com>
2021-03-16 14:52:52 +0100

    google

---
+++ Google/src/layouts/MainLayout.vue
@@ -0,0 +1,166 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header >
+      <q-toolbar 
+          style="background-color:white; color:black">
+        
+
+        <q-toolbar-title>
+  
+        </q-toolbar-title>
+          <div><a href="#">Gmail</a></div>
+          <div style="flex:0.03"></div>
+          <div><a href="#">Images</a></div>
+          <div style="flex:0.03"></div>
+          <div style="flex: 0.02;">
+            <img class="icone" src="./grid.png" alt="">
+          </div>
+          <div style="flex:0.03"></div>
+          <div>
+            <q-btn style="text-transform: none !important;" align="right" class="btn-fixed-width" color="primary" label="Connexion" />
+          </div>
+
+        <div>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+   
+
+    <q-page-container >
+      
+      
+      <router-view />
+    </q-page-container>
+    <div style="background-color:#F2F2F2;color: #6F6F6F;"  class="q-pa-sm footer">
+      <div>France</div>
+      <hr>
+      <div style="padding-top: 10px;" class="row">
+        <div style="flex:0.35;" class="row">
+          <div class="esp"></div>
+          <div style="flex: 0.22;"><a href="#">À propos</a> </div>
+          <div class="esp"></div>
+          <div style="flex: 0.22;"><a href="#">Publicité</a> </div>
+          <div class="esp"></div>
+          <div style="flex: 0.22;"><a href="#">Entreprise</a> </div>
+          <div class="esp"></div>
+          <div style="flex: 1;"><a href="#">Comment fonctionne la recherche Google ?</a> </div>
+        </div>
+        <div style="flex: 0.35;" class="row">
+          <!-- <div style="flex:0.02">
+            <img src="./feuille.jpg" alt="">
+          </div> -->
+          <div style="flex:1;text-align: center;"><a href="#">Neutre en carbone depuis 2007</a> </div>      
+        </div>
+       
+        <div style=" flex: 0.3;" class="row">
+          <div><a href="#">Info consammateurs</a> </div>
+          <div class="esp"></div>
+          <div><a href="#">Confidentialité</a> </div>
+          <div class="esp"></div>
+          <div><a href="#">Conditions</a> </div>
+          <div class="esp"></div>
+          <div><a href="#">Paramètres</a> </div>
+        </div>
+      </div>
+    </div>
+  </q-layout>
+</template>
+
+<script lang="ts">
+// import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'MainLayout',
+  // components: { EssentialLink },
+  setup () {
+    const leftDrawerOpen = ref(false)
+    const essentialLinks = ref(linksData)
+
+    return { leftDrawerOpen, essentialLinks }
+  }
+})
+</script>
+<style lang="scss">
+img{
+  width: 100%;
+}
+.icone{
+  margin-top: 5px;
+}
+.imgg{
+  flex: 0.2;
+  height:20px;
+}
+// .espb{
+//   justify-content: space-around;
+//   text-align: center;
+//   padding: 10px 200px;
+//   align-items: center;
+// }
+.esp{
+  display:flex;
+  flex:0.18;
+  }
+  hr{
+    color: #F2F2F2
+    
+  }
+  a{
+    text-decoration:none;
+    color: unset;
+  }
+  a:hover{
+    text-decoration: black  underline !important;
+    // border: solid 1px red;
+
+  }
+
+</style>
---
+++ Google/src/pages/Index.vue
@@ -0,0 +1,91 @@
+<template>
+  <!-- <q-page class="row items-center justify-evenly">
+    <example-component
+      title="Example component"
+      active
+      :todos="todos"
+      :meta="meta"
+    ></example-component>
+  </q-page> -->
+  
+  <div class="">
+    <div class="row">
+      <div style="flex: 2;"></div>
+      <div style="flex: 2.3;">
+        <img src="./imgg.png" alt="">
+      </div>
+      <div style="flex: 2;"></div>
+    </div>
+    <div style="justify-content: center;" class="row">
+
+      <q-input style="flex:0.35;" rounded outlined v-model="text" >
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>    
+    </div>
+    <div style="justify-content: center;margin-top: 35px;" class="row">
+      <div><q-btn flat class="btng" label="Recherche Google" /></div>
+      <div class="mini"></div>
+      <div><q-btn flat class="btng" label="J'ai de la chance" /></div>
+    </div>
+       <div class="vide"></div>
+  </div>
+  
+  
+</template>
+
+<script lang="ts">
+
+// import { Todo, Meta } from 'components/models'
+// import ExampleComponent from 'components/CompositionComponent.vue'
+// import { defineComponent, ref } from '@vue/composition-api'
+
+// export default defineComponent({
+//   name: 'PageIndex',
+//   components: { ExampleComponent },
+//   setup () {
+//     const todos = ref<Todo[]>([
+//       {
+//         id: 1,
+//         content: 'ct1'
+//       },
+//       {
+//         id: 2,
+//         content: 'ct2'
+//       },
+//       {
+//         id: 3,
+//         content: 'ct3'
+//       },
+//       {
+//         id: 4,
+//         content: 'ct4'
+//       },
+//       {
+//         id: 5,
+//         content: 'ct5'
+//       }
+//     ])
+//     const meta = ref<Meta>({
+//       totalCount: 1200
+//     })
+//     return { todos, meta }
+//   }
+// })
+</script>
+<style lang="scss">
+.vide{
+  height: 332px;
+}
+.mini{
+  flex:0.01;
+}
+.btng{
+  text-transform: none !important;
+  font-weight: 300;
+  color: black;
+  background-color: #F8F9FA;
+}
+
+</style>
